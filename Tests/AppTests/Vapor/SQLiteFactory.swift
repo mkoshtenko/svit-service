3285c705c2147f8dd68b066673804ffef5854e04
Maksym Koshtenko <mkosh@fb.com>
2020-01-19 09:01:41 -0800

    Update dependencies

--- Tests/AppTests/Vapor/SQLiteFactory.swift
+++ Tests/AppTests/Vapor/SQLiteFactory.swift
@@ -4,11 +4,9 @@ import Vapor
 import App
 
 public struct SQLiteFactory: DatabaseFactory {
-    static let filePath = "tests.sqlite"
-
     public let databaseId: DatabaseID = .sqlite
 
     public func configure(_ app: Application) {
-        app.databases.use(.sqlite(configuration: .init(storage: .memory)), as: .sqlite, isDefault: true)
+        app.databases.use(.sqlite(.init(storage: .memory)), as: .sqlite, isDefault: true)
     }
 }
