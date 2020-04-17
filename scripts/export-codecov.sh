b4665fef80c626b76ab05168045793f1dd224cb8
Maksym Koshtenko <maksum.k@gmail.com>
2020-04-17 09:58:31 +0100

    Run tests from container

--- scripts/export-codecov.sh
+++ scripts/export-codecov.sh
@@ -3,8 +3,18 @@
 # export codecoverage data from binary.
 # by default it includes coverage data for all the dependencies and tests,
 # therefore we should use filtering `-ignore-filename-regex`
-xcrun llvm-cov export .build/debug/appPackageTests.xctest/Contents/MacOs/appPackageTests \
+BIN_PATH="$(swift build --show-bin-path)"
+XCTEST_PATH="$(find ${BIN_PATH} -name '*.xctest')"
+
+LLVM_COV='llvm-cov'
+COV_BIN=$XCTEST_PATH
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    f="$(basename $XCTEST_PATH .xctest)"
+    COV_BIN="${COV_BIN}/Contents/MacOS/$f"
+    LLVM_COV='xcrun llvm-cov'
+fi
+
+$LLVM_COV export "${COV_BIN}" \
   -instr-profile=.build/debug/codecov/default.profdata \
   -format lcov >> app.coverage.lcov \
   -ignore-filename-regex="\.build/.*|Tests/.*"
-
--- test.Dockerfile
+++ test.Dockerfile
@@ -6,14 +6,15 @@ ARG env
 RUN apt-get -q update && apt-get -q install -y \
     libssl-dev \
     zlib1g-dev \
+    sqlite3 libsqlite3-dev \
     && rm -r /var/lib/apt/lists/*
 
 WORKDIR /root
 COPY . .
 
 RUN swift package resolve
-RUN swift build
 RUN swift test --enable-code-coverage
 
 RUN bash scripts/export-codecov.sh
-RUN bash <(curl -s https://codecov.io/bash) -J "app" -D ".build/debug"
+
+# RUN bash <(curl -s https://codecov.io/bash) -v -J "app" -D ".build/debug" -t "0ae921f5-d10f-48b8-afcc-3bd80159eaa6" || echo 'Could not submit codecov report'
